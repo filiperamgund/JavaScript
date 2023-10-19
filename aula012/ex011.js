d1693e7209deb6ea4a1fc4454d33b88fdb11179c
filiperamgund <ramgund.filipee@gmail.com>
2023-10-19 12:17:07 -0300

    exercicios atualizados

---
+++ aula012/ex011.js
@@ -0,0 +1,9 @@
+var idade = 30
+if (idade > 30) {
+    console.log('Você possui mais de 30 anos')
+}else if (idade === 30) {
+    console.log('Você possui 30 anos')
+}else {
+    console.log('Você possui menos que 30 anos')
+} 
+
